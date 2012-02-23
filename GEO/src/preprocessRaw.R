68a30eb2f973949e37c053610e12299d32877eae
Levi Waldron <lwaldron.research@gmail.com>
2012-02-22 19:27:22 -0500

    preprocessRaw.R now handles .pcl files and converts them to an ExpressionSet object.

--- GEO/src/preprocessRaw.R
+++ GEO/src/preprocessRaw.R
@@ -19,22 +19,47 @@
 ##
 ## inputargs <- c("./testaffybatch.RData","./testeset.RData","affy::rma")
 ## inputargs <- c("./testaffybatch.RData","./testeset.RData","affy::mas5")
+## inputargs <- c("../data/GSE21815/GSE21815/GSE21815_01norm.pcl","./testeset.RData","affy::mas5")
 
 inputargs <- commandArgs(TRUE)
 print(inputargs)
-sessionInfo()
 
 strInputData		<- inputargs[1]
 strOutputData		<- inputargs[2]
 strProcessingFunction   <- inputargs[3]
 
 strPackage <- strsplit(strProcessingFunction,split="::")[[1]][1]
+library(affy)
 library(strPackage,character.only=TRUE)
+
+sessionInfo()
 
 funPreprocess <- get(strsplit(strProcessingFunction,split="::")[[1]][2])
 
-load(strInputData)
+if(grepl("\\.RData$",strInputData)){
+  ##input is an R object
+  load(strInputData)
+  eset <- funPreprocess(affybatch)
+}
 
-eset <- funPreprocess(affybatch)
+if(grepl("\\.pcl$",strInputData)){
+  ##input is a pcl file
+  mdExprs <- read.delim(strInputData,row.names=1)
+  ##first row and first two columns are cruft
+  mdExprs <- mdExprs[-1,-1:-2]
+  mdExprs <- as.matrix(mdExprs)
+  ##Get rid of anything past the GSM[0-9]+ in the column names
+  colnames(mdExprs) <- gsub("(GSM[0-9]+)(.*)","\\1",colnames(mdExprs))
+  ##log2 transformation if appropriate
+  if( min( mdExprs, na.rm = TRUE ) >= 0 & max( mdExprs, na.rm = TRUE ) >= 50 ) {
+    mdExprs <- log(mdExprs, base = 2)
+  }
+  ##make a fake AnnotatedDataFrame until we can load the real metadata.
+  adf <- new("AnnotatedDataFrame",data=data.frame(sample=1:ncol(mdExprs),row.names=colnames(mdExprs)))
+  ##Create the ExpressionSet
+  eset <- new("ExpressionSet",
+              exprs=mdExprs,
+              phenoData=adf)
+}
 
 save(eset,file=strOutputData,compress="bzip2")
